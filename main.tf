5bb8495f2b0b1ff417c75ff9a9ce03b5efe0090c
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-07 06:29:24 +0000

    did some changes

--- main.tf
+++ main.tf
@@ -1,3 +1,38 @@
+// Key pair
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = var.key
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+//Security Group
+resource "aws_security_group" "sg" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 // EC2
 resource "aws_instance" "ec2" {
   ami                    = var.ami
@@ -5,5 +40,5 @@ resource "aws_instance" "ec2" {
 #   iam_instance_profile   = aws_iam_instance_profile.profile.name
   key_name               = aws_key_pair.default.key_name
   vpc_security_group_ids = [aws_security_group.sg.id]
-  user_data_base64       = filebase64("${path.module}/user-data.sh")
+  user_data_base64       = filebase64("./user-data.sh")
 }
---
+++ modules/var.tf
@@ -0,0 +1,4 @@
+
+variable "key" {
+  default = "t-docker"
+}
