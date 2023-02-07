daa57487282b88953563072817885f93cfd184ec
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-07 09:22:36 +0000

    key

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-// Key pair
-resource "tls_private_key" "key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# // Key pair
+# resource "tls_private_key" "key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-resource "aws_key_pair" "default" {
-  key_name   = var.key
-  public_key = tls_private_key.key.public_key_openssh
-}
+# resource "aws_key_pair" "default" {
+#   key_name   = var.key
+#   public_key = tls_private_key.key.public_key_openssh
+# }
 
-resource "local_file" "tf-key" {
-content  = tls_private_key.key.private_key_pem
-filename = "t-docker"
-}
+# resource "local_file" "tf-key" {
+# content  = tls_private_key.key.private_key_pem
+# filename = "t-docker"
+# }
 
 //Security Group
 resource "aws_security_group" "sg" {
@@ -53,7 +53,7 @@ resource null_resource name {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/.ssh/id_rsa")
+    private_key = file("./t-docker.pem")
     host        = aws_instance.ec2.public_ip
   }
 
