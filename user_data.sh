227d387fbfde1690eef3253685865e6783bb72ff
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-07 06:45:04 +0000

    kdksf

--- user_data.sh
+++ user_data.sh
@@ -1,22 +1,30 @@
-#! /bin/bash
-set -e
+# #! /bin/bash
+# set -e
 
-# Ouput all log
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+# # Ouput all log
+# exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-# Make sure we have all the latest updates when we launch this instance
-sudo apt update -y && sudo apt upgrade -y
+# # Make sure we have all the latest updates when we launch this instance
+# sudo apt update -y && sudo apt upgrade -y
 
-# Install components
-sudo apt install -y docker.io
+# # Install components
+# sudo apt install -y docker.io
 
-# Add credential helper to pull from ECR
-# mkdir -p ~/.docker && chmod 0700 ~/.docker
-# echo '{"credsStore": "ecr-login"}' > ~/.docker/config.json
+# # Add credential helper to pull from ECR
+# # mkdir -p ~/.docker && chmod 0700 ~/.docker
+# # echo '{"credsStore": "ecr-login"}' > ~/.docker/config.json
 
-# Start docker now and enable auto start on boot
-service docker start && chkconfig docker on
+# # Start docker now and enable auto start on boot
+# service docker start && chkconfig docker on
 
-# Allow the ec2-user to run docker commands without sudo
-sudo usermod -a -G docker ubuntu
+# # Allow the ec2-user to run docker commands without sudo
+# sudo usermod -a -G docker ubuntu
+
+#!/bin/bash
+
+# Update package index
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install docker.io -y
+sudo usermod -aG docker ubuntu
 
