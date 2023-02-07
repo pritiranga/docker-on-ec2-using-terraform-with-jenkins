9df5315fbd97903619b74249dd663eaa9838585e
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-07 09:28:03 +0000

    kk

--- output.tf
+++ output.tf
@@ -3,6 +3,6 @@ output "public_ip" {
   value = aws_instance.ec2.public_ip
 }
 
-output "private_pem" {
-  value = nonsensitive(tls_private_key.key.private_key_pem)
-}
+# output "private_pem" {
+#   value = nonsensitive(tls_private_key.key.private_key_pem)
+# }
